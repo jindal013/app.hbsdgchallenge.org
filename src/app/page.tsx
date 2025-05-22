5ef56427d6150fa85c890d16ccf6e1da11eb75e0
Chinmay <chinmayjindal108@gmail.com>
2025-05-22 03:11:41 -0400

    login structure - no functionality rn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import { LoginForm } from "@/components/login-form";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen min-w-screen text-3xl">
-      <p className="text-7xl bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500 bg-clip-text text-transparent min-h-20">SDG Change Challenge 2025</p>
-      <p className="mt-10">WIP... Check back later :)</p>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      {/* <div className="absolute top-0 left-0 w-full h-full bg-hero-banner bg-cover bg-center bg-no-repeat -z-10 before:absolute before:inset-0 before:bg-black/70" /> */}
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
     </div>
   );
 }
