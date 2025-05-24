375b7e7afdcf74220d77f51551ee5f0cc68fbdb3
Chinmay <chinmayjindal108@gmail.com>
2025-05-24 01:56:54 -0400

    feat: new login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { LoginForm } from "@/components/login-form";
+import { LoginForm } from "@/components/login-form"
 
 export default function Home() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh w-full flex-col items-center justify-center p-6 md:p-10 bg-[#F8FAFC] bg-muted">
       {/* <div className="absolute top-0 left-0 w-full h-full bg-hero-banner bg-cover bg-center bg-no-repeat -z-10 before:absolute before:inset-0 before:bg-black/70" /> */}
-      <div className="w-full max-w-sm">
+      <div className="w-full max-w-sm md:max-w-3xl ">
         <LoginForm />
       </div>
     </div>
--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,15 +1,10 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ShineBorder } from "@/components/magicui/shine-border";
+import { MagicCard } from "@/components/magicui/magic-card";
+import { AuroraText } from "@/components/magicui/aurora-text";
 
 export function LoginForm({
   className,
@@ -17,56 +12,82 @@ export function LoginForm({
 }: React.ComponentProps<"div">) {
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card className="relative overflow-hidden max-w-sm w-full">
-        <ShineBorder shineColor={["#A07CFE", "#FE8FB5", "#FFBE7B"]} />
-        <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
-          <CardDescription>
-            Enter your PDSB email to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
+      <Card className="overflow-hidden p-0">
+        <MagicCard gradientColor={"#D9D9D955"} className="p-0"> {/* TODO: Add gradient color with dark theme #262626 */}
+          <CardContent className="grid p-0 md:grid-cols-2">
+            <form className="p-6 md:p-8">
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col items-center text-center">
+                  <AuroraText className="text-[1.8rem] font-semi-bold">SDG Change Challenge</AuroraText>
+                  <p className="text-muted-foreground text-balance text-sm italic mt-1 ">
+                    Login to your mentor account
+                  </p>
                 </div>
-                <Input id="password" type="password" required />
-              </div>
-              <div className="flex flex-col gap-3">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="example@gmail.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto text-sm underline-offset-2 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input id="password" type="password" required />
+                </div>
                 <Button type="submit" className="w-full">
                   Login
                 </Button>
-                <Button variant="outline" className="w-full">
-                  Login with Google
-                </Button>
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                  <span className="bg-card text-muted-foreground relative z-10 px-2">
+                    Or continue with
+                  </span>
+                </div>
+                <div className="grid grid-cols-1 gap-4">
+                  <Button variant="outline" type="button" className="w-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                      <path
+                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                    <span>Login with Google</span>
+                  </Button>
+                </div>
+                <div className="text-center text-sm">
+                  Don&apos;t have an account?{" "}
+                  <a href="#" className="underline underline-offset-4">
+                    Sign up
+                  </a>
+                </div>
               </div>
+            </form>
+            <div className="bg-muted relative hidden md:block">
+              <img
+                src="/hero-banner.jpg"
+                alt="Image"
+                className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              />
             </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
-            </div>
-          </form>
-        </CardContent>
+          </CardContent>
+        </MagicCard>
       </Card>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to Peel District School Board's{" "}
+        <a href="https://www.peelschools.org/privacy-and-terms" target="_blank">
+          Privacy and Terms
+        </a>{" "}
+        policies.
+      </div>
     </div>
   );
 }
