d349afe615c259f7d9731d10f6406f7152321130
Chinmay <chinmayjindal108@gmail.com>
2025-05-24 20:34:05 -0400

    fix: build errors and update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,40 @@
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from "@/components/login-form";
+import FallingText from "@/components/reactbits/FallingText";
 
 export default function Home() {
   return (
-    <div className="flex min-h-svh w-full flex-col items-center justify-center p-6 md:p-10 bg-[#F8FAFC] bg-muted">
-      {/* <div className="absolute top-0 left-0 w-full h-full bg-hero-banner bg-cover bg-center bg-no-repeat -z-10 before:absolute before:inset-0 before:bg-black/70" /> */}
-      <div className="w-full max-w-sm md:max-w-3xl ">
-        <LoginForm />
+    <>
+      <div className="flex min-h-svh w-full flex-col items-center justify-center p-6 md:p-10 bg-muted">
+        {/* <div className="absolute top-0 left-0 w-full h-full bg-hero-banner bg-cover bg-center bg-no-repeat -z-10 before:absolute before:inset-0 before:bg-black/70" /> */}
+        <div className="w-full max-w-sm md:max-w-3xl relative z-20">
+          <LoginForm />
+        </div>
+<div className="fixed inset-0 z-10 pt-[calc((100vh-515px)/8)] hidden min-[1200px]:flex">
+          <FallingText
+            text={`Largest Youth Makerspace. Impactful Change. Endless Innovation. That's SDG. `}
+            highlightWords={[
+              "Makerspace",
+              "Change",
+              "Innovation",
+              "SDG",
+              "Change",
+              "Challenge",
+            ]}
+            highlightClass="highlighted"
+            trigger="auto"
+            backgroundColor="transparent"
+            wireframes={false}
+            gravity={0.3}
+            fontSize="2rem"
+            mouseConstraintStiffness={0.1}
+          />
+        </div>
       </div>
-    </div>
+      <div className="fixed inset-0 z-5">
+        {/* <AuroraBackground>
+          <div />
+        </AuroraBackground> */}
+      </div>
+    </>
   );
 }
